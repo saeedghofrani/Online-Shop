cb579f16a5f720dfd6cb8ba4e8a7f90094da8849
Saeed <sa.ghofraniivari@gmail.com>
2022-12-22 18:32:41 +0330

    Authentication => create permission entity

---
+++ src/entities/AUTH/permission.entity.ts
@@ -0,0 +1,12 @@
+import { Column, Entity, ManyToMany } from 'typeorm';
+import { MainEntity } from '../../common/entities/main.entity';
+import { RoleEntity } from './role.entity';
+
+@Entity({ schema: 'AUTH', name: 'permission' })
+export class PermissionEntity extends MainEntity {
+  @Column()
+  name: string;
+
+  @ManyToMany(() => RoleEntity)
+  roles: RoleEntity[];
+}
