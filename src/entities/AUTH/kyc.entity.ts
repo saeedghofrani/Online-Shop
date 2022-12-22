86566c85a860c5176ccd93dca6de812335e59310
Saeed <sa.ghofraniivari@gmail.com>
2022-12-22 18:32:55 +0330

    Authentication => create kyc entity

---
+++ src/entities/AUTH/kyc.entity.ts
@@ -0,0 +1,18 @@
+import { Column, Entity, OneToOne } from 'typeorm';
+import { MainEntity } from '../../common/entities/main.entity';
+import { UserEntity } from './user.entity';
+
+@Entity({ schema: 'AUTH', name: 'kyc' })
+export class KycEntity extends MainEntity {
+  @Column()
+  father_name: string;
+
+  @Column()
+  national_code: string;
+
+  @Column()
+  birth_date: Date;
+
+  @OneToOne(() => UserEntity)
+  user: UserEntity;
+}
