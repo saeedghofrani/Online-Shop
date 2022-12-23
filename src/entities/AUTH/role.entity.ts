7731231d280b255c98a0dbff10ffc9f9f9b87e89
Saeed <sa.ghofraniivari@gmail.com>
2022-12-23 16:08:11 +0330

    Role => Add role Entity

--- src/entities/AUTH/role.entity.ts
+++ src/entities/AUTH/role.entity.ts
@@ -7,16 +7,24 @@ import {
 } from 'typeorm';
 import { MainEntity } from '../../common/entities/main.entity';
 import { PermissionEntity } from './permission.entity';
+import { UserEntity } from './user.entity';
 
 @Entity({ schema: 'AUTH', name: 'role' })
 export class RoleEntity extends MainEntity {
   @Column()
   name: string;
 
+  @Column()
+  default: boolean;
+
   @DeleteDateColumn({})
   delete_at?: Date;
 
-  @ManyToMany(() => PermissionEntity)
+  @ManyToMany(() => PermissionEntity, (permissions) => permissions.roles)
   @JoinTable({ name: 'role_permissions' })
   permissions: PermissionEntity[];
+
+  @ManyToMany(() => UserEntity, (users) => users.roles)
+  @JoinTable({ name: 'role_permissions' })
+  users: UserEntity[];
 }
