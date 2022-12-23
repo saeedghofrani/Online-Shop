c55f9bed71a08b40c2bfa2032290856a88a9ccaf
Saeed <sa.ghofraniivari@gmail.com>
2022-12-23 16:18:39 +0330

    App => add typeorm root

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,24 @@ import { Module } from '@nestjs/common';
 import { ConfigurationModule } from './config/configuration.module';
 import { RedisModule } from './utils/redis/redis.module';
 import { LoggerModule } from './config/logger/logger.module';
+import { UserModule } from './api/user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { dbConfig } from './config/database/typeorm/postgres/database';
+import { SmsModule } from './utils/sms/sms.module';
+import { EmailModule } from './utils/email/email.module';
+import { PostgresModule } from './config/database/typeorm/postgres/postgres.module';
 
 @Module({
-  imports: [ConfigurationModule, RedisModule, LoggerModule],
+  imports: [
+    ConfigurationModule,
+    RedisModule,
+    LoggerModule,
+    PostgresModule,
+    UserModule,
+    TypeOrmModule.forRoot(dbConfig()),
+    SmsModule,
+    EmailModule,
+  ],
   controllers: [],
   providers: [],
 })
