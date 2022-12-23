836f7cb441c24111fa470e4405d1370647656ef1
Saeed <sa.ghofraniivari@gmail.com>
2022-12-23 16:07:26 +0330

    State => add State entity

---
+++ src/entities/LOCATION/state.entity.ts
@@ -0,0 +1,12 @@
+import { Column, Entity, JoinColumn, OneToOne } from 'typeorm';
+import { MainEntity } from '../../common/entities/main.entity';
+import { CityEntity } from './city.entity';
+
+@Entity({ schema: 'LOCATION', name: 'state' })
+export class StateEntity extends MainEntity {
+  @Column()
+  name: string;
+
+  @OneToOne(() => CityEntity)
+  city: CityEntity;
+}
