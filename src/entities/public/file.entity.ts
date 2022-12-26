687e48a9ab54c01ea32429209a9c020987e177a2
Saeed Ghofrani <sa.ghofraniivari@gmail.com>
2022-12-26 17:52:46 +0330

    File => add file entity

---
+++ src/entities/public/file.entity.ts
@@ -0,0 +1,33 @@
+import { Column, Entity, ManyToOne } from 'typeorm';
+import { MainEntity } from '../../common/entities/main.entity';
+import { UserEntity } from '../AUTH/user.entity';
+import { ProductEntity } from '../PRODUCT/product.entity';
+
+@Entity({ schema: 'public', name: 'file' })
+export class FileEntity extends MainEntity {
+    @Column()
+    file_name: string;
+  
+    @Column()
+    mime_type: string;
+  
+    @Column()
+    ext: string;
+  
+    @Column()
+    size: number;
+  
+    @Column()
+    old_path: string;
+  
+    @Column()
+    new_path: string;
+  
+    @ManyToOne(() => UserEntity, (user) => user.files, {
+      cascade: true,
+    })
+    user: UserEntity;
+
+    @ManyToOne(() => ProductEntity, (product) => product.files)
+    product: ProductEntity;
+}
