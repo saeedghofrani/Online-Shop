9ab8aa6ac34d19cc53dae026bc96502cb1315a4c
Seyyed Mohammad Amir Hekmati <mohammadhekmati1153@gmail.com>
2022-12-26 06:22:56 -0800

    State Module => Develop all state module dependencies
    Develop State service
    state repository
    state create and update dto

---
+++ src/api/location/state/dto/create-state.dto.ts
@@ -0,0 +1,6 @@
+import { ApiProperty } from "@nestjs/swagger";
+
+export class CreateStateDto{
+    @ApiProperty()
+    name:string
+}
---
+++ src/api/location/state/services/state.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from "@nestjs/common";
+import { StateEntity } from "src/entities/LOCATION/state.entity";
+import { UpdateResult } from "typeorm";
+import { CreateStateDto } from "../dto/create-state.dto";
+import { UpdateStateDto } from "../dto/update-state.dto";
+import { StateRepository } from "../repositories/state.repository";
+
+@Injectable()
+export class StateService{
+    constructor(private stateRepository:StateRepository){}
+
+    async createEntity(createEntityDto: CreateStateDto): Promise<StateEntity> {
+        return await this.stateRepository.createEntity(createEntityDto)
+    }
+   async updateEntity(id: string, updateEntityDto: UpdateStateDto): Promise<UpdateResult> {
+        return await this.stateRepository.updateEntity(id,updateEntityDto)
+    }
+   async findOneEntity(id: string): Promise<StateEntity> {
+        return await this.stateRepository.findOneEntity(id)
+    }
+    async findAllEntities(): Promise<StateEntity[]> {
+        return await this.stateRepository.findAllEntities()
+    }
+}
