032b7ffef43fde90291c69c1c285d795b768e535
Saeed <sa.ghofraniivari@gmail.com>
2022-12-27 21:11:20 +0330

    Inventory => inventory module

---
+++ src/api/inventory/inventory.module.ts
@@ -0,0 +1,16 @@
+import { Module } from "@nestjs/common";
+import { AuthModule } from "../auth/auth.module";
+import { ProviderController } from "./provider/controller/provider.controller";
+import { ProviderRepository } from "./provider/repository/provider.repository";
+import { ProviderService } from "./provider/service/provider.service";
+import { SummaryController } from "./summary/controller/summary.controller";
+import { SummaryRepository } from "./summary/repository/summary.repository";
+import { SummaryService } from "./summary/service/summary.service";
+
+@Module({
+    imports: [AuthModule],
+    controllers: [ProviderController, SummaryController],
+    providers: [ProviderService, ProviderRepository, SummaryService, SummaryRepository],
+    exports: [],
+})
+export class InventoryModuel {}
