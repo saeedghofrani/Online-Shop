a7910b443ab6b14435ac01c6e95f00fd8cb2569e
Saeed <sa.ghofraniivari@gmail.com>
2022-12-27 21:08:36 +0330

    Provider Entity => create provider entity

---
+++ src/entities/inventory/provider.entity.ts
@@ -0,0 +1,27 @@
+import { Column, Entity, ManyToOne, OneToMany, Unique } from 'typeorm';
+import { MainEntity } from '../../common/entities/main.entity';
+import { UserEntity } from '../AUTH/user.entity';
+import { ProviderStatus } from './enum/provider-status.enum';
+import { SummaryEntity } from './summary.entity';
+
+@Entity({ schema: 'inventory', name: 'provider' })
+@Unique(['latitude', 'longitude'])
+export class ProviderEntity extends MainEntity {
+  @Column({ unique: true })
+  address: string;
+
+  @Column()
+  latitude: string;
+
+  @Column()
+  longitude: string;
+
+  @Column({ type: 'enum', enum: ProviderStatus, default: ProviderStatus.PENDING})
+  status: ProviderStatus
+
+  @OneToMany(() => SummaryEntity, (summaries) => summaries.provider)
+  summaries: SummaryEntity[];
+
+  @ManyToOne(() => UserEntity, (user) => user.providers)
+  user: UserEntity;
+}
