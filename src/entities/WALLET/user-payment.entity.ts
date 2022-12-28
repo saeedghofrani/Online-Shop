62a9d434850f0d5246010d9366fc7decdcd23468
Saeed Ghofrani <sa.ghofraniivari@gmail.com>
2022-12-28 19:22:36 +0330

    wallet => create user payment entity

---
+++ src/entities/WALLET/user-payment.entity.ts
@@ -0,0 +1,24 @@
+import { ColumnNumericTransformer } from 'src/common/classes/column-numeric-transformer.class';
+import { Column, Entity, ManyToOne } from 'typeorm';
+import { MainEntity } from '../../common/entities/main.entity';
+import { UserEntity } from '../AUTH/user.entity';
+
+@Entity({ schema: 'wallet', name: 'user-payment' })
+export class UserPaymentEntity extends MainEntity {
+    @ManyToOne(() => UserEntity, user => user.user_payments)
+    user: UserEntity;
+
+    @Column('numeric', {
+        transformer: new ColumnNumericTransformer(),
+    })
+    price: number;
+
+    @Column({ type: 'boolean', default: false })
+    status: boolean;
+
+    @Column()
+    do_date: Date;
+
+    @Column()
+    serial: string;
+}
