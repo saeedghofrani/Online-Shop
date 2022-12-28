edebc9eb0c6fcf0139df53971bb9d82229aa1c14
Saeed Ghofrani <sa.ghofraniivari@gmail.com>
2022-12-28 19:22:47 +0330

    wallet => create pricing entity

---
+++ src/entities/WALLET/pricing.entity.ts
@@ -0,0 +1,19 @@
+import { ColumnNumericTransformer } from 'src/common/classes/column-numeric-transformer.class';
+import { Column, Entity, ManyToOne, OneToMany } from 'typeorm';
+import { MainEntity } from '../../common/entities/main.entity';
+import { ProductEntity } from '../PRODUCT/product.entity';
+import { OrderEntity } from './order.entity';
+
+@Entity({ schema: 'wallet', name: 'pricing' })
+export class PricingEntity extends MainEntity {
+    @Column('numeric', {
+        transformer: new ColumnNumericTransformer(),
+    })
+    price: number;
+
+    @ManyToOne(() => ProductEntity, product => product.pricings)
+    product: ProductEntity;
+    
+  @OneToMany(() => OrderEntity, (orders) => orders.price)
+  orders: OrderEntity[];
+}
