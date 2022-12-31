8c39c3845e05ae9064ef58354306899d42ecc08a
Saeed Ghofrani <sa.ghofraniivari@gmail.com>
2022-12-31 19:47:52 +0330

    Mongoose => add mongoose config

---
+++ src/config/database/mongoose/mongoose.module.ts
@@ -0,0 +1,7 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
+@Module({
+    imports: [MongooseModule.forRoot('mongodb://hampa:HmpCo_2022_@192.168.10.200:27017/history?connectTimeoutMS=10000&authSource=admin&authMechanism=SCRAM-SHA-1')],
+})
+export class MopngooseModule {}
