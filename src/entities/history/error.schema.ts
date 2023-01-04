ebab9241c7ad01fd644822137ac2ac9c3670f58e
Saeed <sa.ghofraniivari@gmail.com>
2023-01-04 22:38:38 +0330

    Logs => Error Schema

---
+++ src/entities/history/error.schema.ts
@@ -0,0 +1,23 @@
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { HydratedDocument } from "mongoose";
+
+export type ErrorHistoryDocument = HydratedDocument<ErrorHistory>;
+
+@Schema({ timestamps: true })
+export class ErrorHistory {
+  @Prop({ required: true })
+  status: string;
+
+  @Prop({ required: true })
+  error: string;
+
+  @Prop({ required: true })
+  route: string;
+
+  @Prop({ required: true })
+  path: string;
+
+  @Prop({ required: true })
+  methode: string;
+}
+export const ErrorHistorySchema = SchemaFactory.createForClass(ErrorHistory);
