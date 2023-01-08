90878295b3ec38ae723ae29b5e6752e10e4a31a2
Saeed <sa.ghofraniivari@gmail.com>
2023-01-08 09:41:43 +0330

    Response => Change Interceptor

--- src/common/interceptors/global-response.interceptor.ts
+++ src/common/interceptors/global-response.interceptor.ts
@@ -1,7 +1,7 @@
 import { CallHandler, ExecutionContext, NestInterceptor } from '@nestjs/common';
 import { map, Observable } from 'rxjs';
 import { GlobalResponseClass } from '../classes/global-response.class';
-import { Request } from 'express';
+import { Request, Response } from 'express';
 
 export class ResponseOkInterceptor implements NestInterceptor {
   intercept(
@@ -11,8 +11,12 @@ export class ResponseOkInterceptor implements NestInterceptor {
     | Observable<GlobalResponseClass>
     | Promise<Observable<GlobalResponseClass>> {
     const req: Request = context.switchToHttp().getRequest();
+    const res: Response = context.switchToHttp().getResponse();
+    let statusCode: number = res.statusCode;
+    let status: string;
+    statusCode < 400 ? status = 'success' : status = 'failed'
     return next
       .handle()
-      .pipe(map((data) => new GlobalResponseClass(req, data)));
+      .pipe(map((data) => new GlobalResponseClass(status, statusCode, data)));
   }
 }
