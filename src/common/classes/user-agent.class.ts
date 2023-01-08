570ec53e9cb27922a0a820e857145d90913b646f
Saeed <sa.ghofraniivari@gmail.com>
2023-01-08 09:40:49 +0330

    Logger => Add User Agent Class

---
+++ src/common/classes/user-agent.class.ts
@@ -0,0 +1,36 @@
+export class UserAgentClass {
+
+    ua: string
+    browser: Browser
+    engine: Engine
+    os: Os
+    device: Device
+    cpu: Cpu
+
+}
+
+class Browser {
+    name: string
+    version: string
+    major?: string
+}
+
+class Engine {
+    name: string
+    version: string
+}
+
+class Os {
+    name: string
+    version: string
+}
+
+class Device {
+    model: string
+    type: string
+    vendor: string
+}
+
+class Cpu {
+    architecture: string
+}
