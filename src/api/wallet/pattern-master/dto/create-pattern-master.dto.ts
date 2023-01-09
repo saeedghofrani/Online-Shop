e9d7dbfb3dfdcea7919bd145c7f50e2b9ecfff1d
Saeed Ghofrani <sa.ghofraniivari@gmail.com>
2023-01-09 17:05:50 +0330

    Pattern Master => Pattern Master Module

---
+++ src/api/wallet/pattern-master/dto/create-pattern-master.dto.ts
@@ -0,0 +1,20 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { installmentType } from 'src/entities/WALLET/enum/installment-type.enum';
+import { PenaltyType } from 'src/entities/WALLET/enum/penalty-type.enum';
+
+export class CreatePatternMasterDto {
+    @ApiProperty()
+    amount: number;
+
+    @ApiProperty()
+    interest_rates: number;
+
+    @ApiProperty()
+    installmentType: installmentType;
+
+    @ApiProperty()
+    penaltyType: PenaltyType;
+
+    @ApiProperty()
+    penalty: number;
+}
---
+++ src/api/wallet/pattern-master/service/pattern-master.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@nestjs/common';
+import { PatternMasterEntity } from 'src/entities/WALLET/pattern-master.entity';
+import { UpdateResult } from 'typeorm';
+import { CreatePatternMasterDto } from '../dto/create-pattern-master.dto';
+import { UpdatePatternMasterDto } from '../dto/update-pattern-master.dto';
+import { PatternMasterRepository } from '../repository/pattern-master.repository';
+
+@Injectable()
+export class PatternMasterService {
+  constructor(
+    private patternMasterRepository: PatternMasterRepository,
+  ) {}
+  async createEntity(
+    createEntityDto: CreatePatternMasterDto,
+  ): Promise<PatternMasterEntity> {
+    try {
+      return await this.patternMasterRepository.createEntity(createEntityDto);
+    } catch (e) {}
+  }
+
+  async findAllEntities(): Promise<PatternMasterEntity[]> {
+    try {
+      return await this.patternMasterRepository.findAllEntities();
+    } catch (e) {}
+  }
+
+  async findOneEntity(id: string): Promise<PatternMasterEntity> {
+    try {
+      return await this.patternMasterRepository.findOneEntity(id);
+    } catch (e) {}
+  }
+
+  async updateEntity(
+    id: string,
+    updateEntityDto: UpdatePatternMasterDto,
+  ): Promise<UpdateResult> {
+    try {
+      return await this.patternMasterRepository.updateEntity(id, updateEntityDto);
+    } catch (e) {}
+  }
+}
