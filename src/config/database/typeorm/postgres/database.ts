400c406a1ea094d3a2935fcd36d30e2e96cc9547
Saeed Ghofrani <sa.ghofraniivari@gmail.com>
2023-09-28 10:19:15 +0330

    db config => remove comments

--- src/config/database/typeorm/postgres/database.ts
+++ src/config/database/typeorm/postgres/database.ts
@@ -32,41 +32,4 @@ export const dbConfig = (): DataSourceOptions => ({
     connectionTimeoutMillis: 2000
   }
 });
-
-// readonly cache?: boolean | {
-//   /**
-//    * Type of caching.
-//    *
-//    * - "database" means cached values will be stored in the separate table in database. This is default value.
-//    * - "redis" means cached values will be stored inside redis. You must provide redis connection options.
-//    */
-//   readonly type?: "database" | "redis" | "ioredis" | "ioredis/cluster";
-//   /**
-//    * Factory function for custom cache providers that implement QueryResultCache.
-//    */
-//   readonly provider?: (connection: DataSource) => QueryResultCache;
-//   /**
-//    * Configurable table name for "database" type cache.
-//    * Default value is "query-result-cache"
-//    */
-//   readonly tableName?: string;
-//   /**
-//    * Used to provide redis connection options.
-//    */
-//   readonly options?: any;
-//   /**
-//    * If set to true then queries (using find methods and QueryBuilder's methods) will always be cached.
-//    */
-//   readonly alwaysEnabled?: boolean;
-//   /**
-//    * Time in milliseconds in which cache will expire.
-//    * This can be setup per-query.
-//    * Default value is 1000 which is equivalent to 1 second.
-//    */
-//   readonly duration?: number;
-//   /**
-//    * Used to specify if cache errors should be ignored, and pass through the call to the Database.
-//    */
-//   readonly ignoreErrors?: boolean;
-// };
 export default dbConfig;
