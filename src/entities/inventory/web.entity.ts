851c1c6bb366e330d72f80642d26b70e2839d70c
Saeed Ghofrani <sa.ghofraniivari@gmail.com>
2023-09-30 04:01:20 +0330

    web => create web entity

---
+++ src/entities/inventory/web.entity.ts
@@ -0,0 +1,23 @@
+import {
+    Column,
+    DeleteDateColumn,
+    Entity,
+    ManyToOne
+} from 'typeorm';
+import { MainEntity } from '../../common/entities/main.entity';
+import { ProviderEntity } from './provider.entity';
+
+@Entity({ schema: 'inventory', name: 'web' })
+export class WebEntity extends MainEntity {
+    @Column()
+    name: string;
+
+    @Column()
+    original_name: string;
+
+    @ManyToOne(() => ProviderEntity, (provider) => provider.web)
+    provider: ProviderEntity;
+    
+    @DeleteDateColumn({})
+    delete_at?: Date;
+}
