553aab84f3c16b88b585d83933bedbe34350ff8c
Saeed Ghofrani <sa.ghofraniivari@gmail.com>
2023-10-03 03:15:28 +0330

    test

--- src/common/helper/env.helper.ts
+++ src/common/helper/env.helper.ts
@@ -1,7 +1,7 @@
 import { resolve } from 'path';
 
 export function getEnvPath(): string {
-  const prefix = './src/common/envs';
+  const prefix = 'src/common/envs';
   const env: string | undefined = process.env.APP_MODE;
   const filename: string = env
     ? `${prefix}${env}.env`
